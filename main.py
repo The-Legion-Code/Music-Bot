175722e703057f9d5943bfe6922cfd4f3b78853f
Atomic748 <75083820+Atomic748@users.noreply.github.com>
2022-06-09 20:24:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run("OTg0MzAxNjcxNzY3MjQ0ODEw.GnAPK8.sNKv2JdnFrLHsO6c7dtGQaDFMw3pb-EBYcSOJo")
+client.run("OTg0MzAxNjcxNzY3MjQ0ODEw.Gs8gX2.81sP44wpWY_naVTbhYA-N6RABEpeP1iv2VrIjQ")
