e9a24f410761259fbe83c582da6962788cd71818
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-06-09 22:38:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import music
-
+import os
+ 
 cogs = [music]
 
 client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
@@ -9,4 +10,4 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run("OTg0MzAxNjcxNzY3MjQ0ODEw.Gs8gX2.81sP44wpWY_naVTbhYA-N6RABEpeP1iv2VrIjQ")
+client.run(os.environ["TOKEN"])
